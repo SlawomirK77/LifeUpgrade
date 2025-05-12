0f8793cd34e5dfddc4fdcb2a2489659b4284c80a
Slawek <januszbak34@gmail.com>
2025-05-12 10:53:03 +0200

    added efcore files

---
+++ life-upgrade.Infrastructure/Migrations/20250409105921_AddedWebShop.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LifeUpgrade.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedWebShop : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WebShops",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WebShops", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WebShops_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WebShops_ProductId",
+                table: "WebShops",
+                column: "ProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WebShops");
+        }
+    }
+}
