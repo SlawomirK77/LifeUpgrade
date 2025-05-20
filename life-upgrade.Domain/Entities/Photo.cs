7f4193f82fd9ad10b0e0c1904a7321218ce90874
Slawek <januszbak34@gmail.com>
2025-05-20 15:56:39 +0200

    photo description is not required anymore

--- life-upgrade.Domain/Entities/Photo.cs
+++ life-upgrade.Domain/Entities/Photo.cs
@@ -4,7 +4,7 @@ public class Photo
 {
     public Guid Id { get; init; }
     public List<byte> Bytes { get; set; } = default!;
-    public string Description { get; set; } = default!;
+    public string? Description { get; set; }
     public string FileExtension { get; set; } = default!;
     public decimal Size { get; set; } = default!;
     public Guid ProductId { get; set; } = default!;
